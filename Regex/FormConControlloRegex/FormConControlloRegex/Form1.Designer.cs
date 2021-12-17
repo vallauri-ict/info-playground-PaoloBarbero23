4b96a67e5b0b946e4b9b32f230e39dda717867f8
Paolo Barbero <p.barbero.1701@vallauri.edu>
2021-12-17 21:51:20 +0100

    Esercizio controllo regex completato

--- Regex/FormConControlloRegex/FormConControlloRegex/Form1.Designer.cs
+++ Regex/FormConControlloRegex/FormConControlloRegex/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblNome = new System.Windows.Forms.Label();
             this.lblCognome = new System.Windows.Forms.Label();
             this.txtNome = new System.Windows.Forms.TextBox();
@@ -46,157 +47,250 @@
             this.lblUserName = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.lblPassword = new System.Windows.Forms.Label();
+            this.btnControllo = new System.Windows.Forms.Button();
+            this.errorProviderNome = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderCognome = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderEta = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderCAP = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderCitta = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderMail = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderCF = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderUsername = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderPassword = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderNome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCognome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderEta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCAP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCitta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderMail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUsername)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPassword)).BeginInit();
             this.SuspendLayout();
             // 
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Location = new System.Drawing.Point(12, 10);
+            this.lblNome.Location = new System.Drawing.Point(16, 12);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.Size = new System.Drawing.Size(45, 17);
             this.lblNome.TabIndex = 0;
             this.lblNome.Text = "Nome";
             // 
             // lblCognome
             // 
             this.lblCognome.AutoSize = true;
-            this.lblCognome.Location = new System.Drawing.Point(12, 36);
+            this.lblCognome.Location = new System.Drawing.Point(16, 44);
+            this.lblCognome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCognome.Name = "lblCognome";
-            this.lblCognome.Size = new System.Drawing.Size(52, 13);
+            this.lblCognome.Size = new System.Drawing.Size(68, 17);
             this.lblCognome.TabIndex = 1;
             this.lblCognome.Text = "Cognome";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(70, 7);
+            this.txtNome.Location = new System.Drawing.Point(93, 9);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.Size = new System.Drawing.Size(132, 22);
             this.txtNome.TabIndex = 2;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
             // 
             // txtCognome
             // 
-            this.txtCognome.Location = new System.Drawing.Point(70, 33);
+            this.txtCognome.Location = new System.Drawing.Point(93, 41);
+            this.txtCognome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCognome.Name = "txtCognome";
-            this.txtCognome.Size = new System.Drawing.Size(100, 20);
+            this.txtCognome.Size = new System.Drawing.Size(132, 22);
             this.txtCognome.TabIndex = 3;
+            this.txtCognome.TextChanged += new System.EventHandler(this.txtCognome_TextChanged);
             // 
             // txtEta
             // 
-            this.txtEta.Location = new System.Drawing.Point(70, 59);
+            this.txtEta.Location = new System.Drawing.Point(93, 73);
+            this.txtEta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEta.Name = "txtEta";
-            this.txtEta.Size = new System.Drawing.Size(100, 20);
+            this.txtEta.Size = new System.Drawing.Size(132, 22);
             this.txtEta.TabIndex = 5;
+            this.txtEta.TextChanged += new System.EventHandler(this.txtEta_TextChanged);
             // 
             // lblEta
             // 
             this.lblEta.AutoSize = true;
-            this.lblEta.Location = new System.Drawing.Point(12, 62);
+            this.lblEta.Location = new System.Drawing.Point(16, 76);
+            this.lblEta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEta.Name = "lblEta";
-            this.lblEta.Size = new System.Drawing.Size(23, 13);
+            this.lblEta.Size = new System.Drawing.Size(29, 17);
             this.lblEta.TabIndex = 4;
             this.lblEta.Text = "Età";
             // 
             // txtCAP
             // 
-            this.txtCAP.Location = new System.Drawing.Point(70, 85);
+            this.txtCAP.Location = new System.Drawing.Point(93, 105);
+            this.txtCAP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCAP.Name = "txtCAP";
-            this.txtCAP.Size = new System.Drawing.Size(100, 20);
+            this.txtCAP.Size = new System.Drawing.Size(132, 22);
             this.txtCAP.TabIndex = 7;
+            this.txtCAP.TextChanged += new System.EventHandler(this.txtCAP_TextChanged);
             // 
             // lblCAP
             // 
             this.lblCAP.AutoSize = true;
-            this.lblCAP.Location = new System.Drawing.Point(12, 88);
+            this.lblCAP.Location = new System.Drawing.Point(16, 108);
+            this.lblCAP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCAP.Name = "lblCAP";
-            this.lblCAP.Size = new System.Drawing.Size(28, 13);
+            this.lblCAP.Size = new System.Drawing.Size(35, 17);
             this.lblCAP.TabIndex = 6;
             this.lblCAP.Text = "CAP";
             // 
             // txtCittà
             // 
-            this.txtCittà.Location = new System.Drawing.Point(70, 111);
+            this.txtCittà.Location = new System.Drawing.Point(93, 137);
+            this.txtCittà.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCittà.Name = "txtCittà";
-            this.txtCittà.Size = new System.Drawing.Size(100, 20);
+            this.txtCittà.Size = new System.Drawing.Size(132, 22);
             this.txtCittà.TabIndex = 9;
+            this.txtCittà.TextChanged += new System.EventHandler(this.txtCittà_TextChanged);
             // 
             // lblCittà
             // 
             this.lblCittà.AutoSize = true;
-            this.lblCittà.Location = new System.Drawing.Point(12, 114);
+            this.lblCittà.Location = new System.Drawing.Point(16, 140);
+            this.lblCittà.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCittà.Name = "lblCittà";
-            this.lblCittà.Size = new System.Drawing.Size(28, 13);
+            this.lblCittà.Size = new System.Drawing.Size(36, 17);
             this.lblCittà.TabIndex = 8;
             this.lblCittà.Text = "Città";
             // 
             // txtMail
             // 
-            this.txtMail.Location = new System.Drawing.Point(70, 137);
+            this.txtMail.Location = new System.Drawing.Point(93, 169);
+            this.txtMail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMail.Name = "txtMail";
-            this.txtMail.Size = new System.Drawing.Size(100, 20);
+            this.txtMail.Size = new System.Drawing.Size(132, 22);
             this.txtMail.TabIndex = 11;
+            this.txtMail.TextChanged += new System.EventHandler(this.txtMail_TextChanged);
             // 
             // lblMail
             // 
             this.lblMail.AutoSize = true;
-            this.lblMail.Location = new System.Drawing.Point(12, 140);
+            this.lblMail.Location = new System.Drawing.Point(16, 172);
+            this.lblMail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMail.Name = "lblMail";
-            this.lblMail.Size = new System.Drawing.Size(26, 13);
+            this.lblMail.Size = new System.Drawing.Size(33, 17);
             this.lblMail.TabIndex = 10;
             this.lblMail.Text = "Mail";
             // 
             // txtCF
             // 
-            this.txtCF.Location = new System.Drawing.Point(70, 163);
+            this.txtCF.Location = new System.Drawing.Point(93, 201);
+            this.txtCF.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCF.Name = "txtCF";
-            this.txtCF.Size = new System.Drawing.Size(100, 20);
+            this.txtCF.Size = new System.Drawing.Size(132, 22);
             this.txtCF.TabIndex = 13;
+            this.txtCF.TextChanged += new System.EventHandler(this.txtCF_TextChanged);
             // 
             // lblCF
             // 
             this.lblCF.AutoSize = true;
-            this.lblCF.Location = new System.Drawing.Point(12, 166);
+            this.lblCF.Location = new System.Drawing.Point(16, 204);
+            this.lblCF.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCF.Name = "lblCF";
-            this.lblCF.Size = new System.Drawing.Size(29, 13);
+            this.lblCF.Size = new System.Drawing.Size(37, 17);
             this.lblCF.TabIndex = 12;
             this.lblCF.Text = "C. F.";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(70, 189);
+            this.txtUserName.Location = new System.Drawing.Point(93, 233);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.Size = new System.Drawing.Size(132, 22);
             this.txtUserName.TabIndex = 15;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
             // 
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(12, 192);
+            this.lblUserName.Location = new System.Drawing.Point(16, 236);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(58, 13);
+            this.lblUserName.Size = new System.Drawing.Size(77, 17);
             this.lblUserName.TabIndex = 14;
             this.lblUserName.Text = "User name";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(70, 215);
+            this.txtPassword.Location = new System.Drawing.Point(93, 265);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.Size = new System.Drawing.Size(132, 22);
             this.txtPassword.TabIndex = 17;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(12, 218);
+            this.lblPassword.Location = new System.Drawing.Point(16, 268);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.Size = new System.Drawing.Size(69, 17);
             this.lblPassword.TabIndex = 16;
             this.lblPassword.Text = "Password";
             // 
+            // btnControllo
+            // 
+            this.btnControllo.Location = new System.Drawing.Point(19, 310);
+            this.btnControllo.Name = "btnControllo";
+            this.btnControllo.Size = new System.Drawing.Size(179, 41);
+            this.btnControllo.TabIndex = 18;
+            this.btnControllo.Text = "Controllo";
+            this.btnControllo.UseVisualStyleBackColor = true;
+            this.btnControllo.Click += new System.EventHandler(this.btnControllo_Click);
+            // 
+            // errorProviderNome
+            // 
+            this.errorProviderNome.ContainerControl = this;
+            // 
+            // errorProviderCognome
+            // 
+            this.errorProviderCognome.ContainerControl = this;
+            // 
+            // errorProviderEta
+            // 
+            this.errorProviderEta.ContainerControl = this;
+            // 
+            // errorProviderCAP
+            // 
+            this.errorProviderCAP.ContainerControl = this;
+            // 
+            // errorProviderCitta
+            // 
+            this.errorProviderCitta.ContainerControl = this;
+            // 
+            // errorProviderMail
+            // 
+            this.errorProviderMail.ContainerControl = this;
+            // 
+            // errorProviderCF
+            // 
+            this.errorProviderCF.ContainerControl = this;
+            // 
+            // errorProviderUsername
+            // 
+            this.errorProviderUsername.ContainerControl = this;
+            // 
+            // errorProviderPassword
+            // 
+            this.errorProviderPassword.ContainerControl = this;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 295);
+            this.ClientSize = new System.Drawing.Size(316, 363);
+            this.Controls.Add(this.btnControllo);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.txtUserName);
@@ -215,8 +309,19 @@
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.lblCognome);
             this.Controls.Add(this.lblNome);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderNome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCognome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderEta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCAP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCitta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderMail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderCF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderUsername)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPassword)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -242,6 +347,16 @@
         private System.Windows.Forms.Label lblUserName;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnControllo;
+        private System.Windows.Forms.ErrorProvider errorProviderNome;
+        private System.Windows.Forms.ErrorProvider errorProviderCognome;
+        private System.Windows.Forms.ErrorProvider errorProviderEta;
+        private System.Windows.Forms.ErrorProvider errorProviderCAP;
+        private System.Windows.Forms.ErrorProvider errorProviderCitta;
+        private System.Windows.Forms.ErrorProvider errorProviderMail;
+        private System.Windows.Forms.ErrorProvider errorProviderCF;
+        private System.Windows.Forms.ErrorProvider errorProviderUsername;
+        private System.Windows.Forms.ErrorProvider errorProviderPassword;
     }
 }
 
--- Regex/FormConControlloRegex/FormConControlloRegex/Form1.cs
+++ Regex/FormConControlloRegex/FormConControlloRegex/Form1.cs
@@ -14,23 +14,126 @@ namespace FormConControlloRegex
 {
     public partial class Form1 : Form
     {
-
+        Regex rgx_nome;
+        Regex rgx_età;
+        Regex rgx_CAP;
+        Regex rgx_citta;
+        Regex rgx_mail;
+        Regex rgx_CF;
+        Regex rgx_username;
+        Regex rgx_password;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void errore(TextBox txt)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            
+            rgx_nome = new Regex(@"^([a-zA-Z]{3,})$");//cognome è uguale
+            rgx_età = new Regex(@"^\d{1,3}$");//non funziona
+            rgx_CAP = new Regex(@"^\d{5}$");
+            rgx_citta = new Regex(@"^[a-zA-Z]{3,} [a-zA-Z]{2}$");
+            rgx_mail = new Regex(@"^(\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)(,\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)*$");
+            rgx_CF = new Regex(@"^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$");
+            rgx_username = new Regex(@"^[a-zA-Z]\w{4,}$");
+            rgx_password = new Regex(@"^(\w|[-!+]){8,15}$");//non funziona
+        }
 
-            txt.BorderStyle = BorderStyle.None;
-            Pen p = new Pen(Color.Red);
-            Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+        private void btnControllo_Click(object sender, EventArgs e)
+        {
+            if (rgx_nome.IsMatch(txtNome.Text) && rgx_nome.IsMatch(txtCognome.Text) && rgx_età.IsMatch(txtEta.Text) && rgx_CAP.IsMatch(txtCAP.Text) && rgx_citta.IsMatch(txtCittà.Text) && rgx_mail.IsMatch(txtMail.Text) && rgx_CF.IsMatch(txtCF.Text) && rgx_username.IsMatch(txtUserName.Text) && rgx_password.IsMatch(txtPassword.Text))
+            {
+
+
+                string[] mail = txtMail.Text.Split(',');
+                string output = "";
+                for (int i = 0; i < mail.Length; i++)
+                    output += mail[i] + "\n";
+                MessageBox.Show("Mail: \n" + output);
+            }
+            else
+                MessageBox.Show("Errore nella compilazione di alcuni campi");
+
+
+
+
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_nome.IsMatch(txtNome.Text) && txtNome.Text != "")
+                errorProviderNome.SetError((Control)sender, "Il nome deve avere solo lettere e di almeno 3 caratteri");
+            else
+                errorProviderNome.Clear();
             
-            
-            int variance = 3;
-            g.DrawRectangle(p, new Rectangle(txt.Location.X - variance, txt.Location.Y - variance, txt.Width + variance, txt.Height + variance));
+        }
+
+        private void txtCognome_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_nome.IsMatch(txtCognome.Text) && txtCognome.Text != "")
+                errorProviderCognome.SetError((Control)sender, "Il cognome deve avere solo lettere e di almeno 3 caratteri");
+            else
+                errorProviderCognome.Clear();
+
+        }
+
+        private void txtEta_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_età.IsMatch(txtEta.Text) && txtEta.Text != "")
+                errorProviderEta.SetError((Control)sender, "l'età dev essere compresa fra 0 e 120");
+            else
+                errorProviderEta.Clear();
+
+        }
+
+        private void txtCAP_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_CAP.IsMatch(txtCAP.Text) && txtCAP.Text != "")
+                errorProviderCAP.SetError((Control)sender, "CAP deve avere 5 cifre");
+            else
+                errorProviderCAP.Clear();
+
+        }
+
+        private void txtCittà_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_citta.IsMatch(txtCittà.Text) && txtCittà.Text != "")
+                errorProviderCitta.SetError((Control)sender, "La città deve avere 3 lettera e seguita da uno spazio e 2 lettere. Es. XXXXX XX");
+            else
+                errorProviderCitta.Clear();
+        }
+
+        private void txtMail_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_mail.IsMatch(txtMail.Text) && txtMail.Text != "")
+                errorProviderMail.SetError((Control)sender, "La mail deve essere formata: carattere1@dominio2.dominio1  Il carattere 1 può essere seguito da .carattere2 (infinite volte) e dopo dominio2 ci possono essere infiniti .dominio3. Possono essere aggiunte più mail separate fra loro da una virgola(,) senza spazi");
+            else
+                errorProviderMail.Clear();
+        }
+
+        private void txtCF_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_CF.IsMatch(txtCF.Text) && txtCF.Text != "")
+                errorProviderCF.SetError((Control)sender, "Codice fiscale errato");
+            else
+                errorProviderCF.Clear();
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_username.IsMatch(txtUserName.Text) && txtUserName.Text != "")
+                errorProviderUsername.SetError((Control)sender, "Lo user name deve avere almeno 5 caratteri di cui il primo alfabetico e gli altri alfanumerici");
+            else
+                errorProviderUsername.Clear();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (!rgx_password.IsMatch(txtPassword.Text) && txtPassword.Text != "")
+                errorProviderPassword.SetError((Control)sender, "La password deve avere dagli 8 a 15 caratteri alfanumerici e deve contenere uno di questi caratteri - ! +!");
+            else
+                errorProviderPassword.Clear();
         }
     }
 
